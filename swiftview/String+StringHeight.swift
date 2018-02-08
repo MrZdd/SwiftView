7ea3866ddac95dc69ec21c790918c9c4efe94a37
Macbook <469648303@qq.com>
2018-02-08 15:13:04 +0800

    swift

---
+++ swiftview/String+StringHeight.swift
@@ -0,0 +1,28 @@
+//
+//  String+StringHeight.swift
+//  swiftdemo2
+//
+//  Created by Macbook on 2018/2/8.
+//  Copyright © 2018年 Macbook. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class String_StringHeight: NSString {
+
+}
+extension String {
+    func ga_heightForComment(fontSize: CGFloat, width: CGFloat) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        return ceil(rect.height)
+    }
+    
+    
+    func ga_widthForComment(fontSize: CGFloat, height: CGFloat = 15) -> CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let rect = NSString(string: self).boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: height), options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        return ceil(rect.width)
+    }
+
+}
